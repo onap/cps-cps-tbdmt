f1cb978edbd4fda0bf802c2d511e364435073dc6
krishnaa96 <krishna.moorthy6@wipro.com>
2021-02-26 13:02:24 +0530

    Add cps-tdmt-service code
    Issue-ID: CPS-243
    Signed-off-by: krishnaa96 <krishna.moorthy6@wipro.com>
    Change-Id: I466bf00586dc8c11fcc50b673cf58d46ec461089

---
+++ cps-tbdmt-service/src/main/java/org/onap/cps/tbdmt/service/ExecutionBusinessLogic.java
@@ -0,0 +1,81 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP
+ * ================================================================================
+ * Copyright (C) 2021 Wipro Limited.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.tbdmt.service;
+
+import com.hubspot.jinjava.Jinjava;
+import java.util.Map;
+import java.util.Optional;
+import org.onap.cps.tbdmt.client.CpsRestClient;
+import org.onap.cps.tbdmt.db.TemplateRepository;
+import org.onap.cps.tbdmt.exception.CpsClientException;
+import org.onap.cps.tbdmt.exception.ExecuteException;
+import org.onap.cps.tbdmt.exception.TemplateNotFoundException;
+import org.onap.cps.tbdmt.model.AppConfiguration;
+import org.onap.cps.tbdmt.model.ExecutionRequest;
+import org.onap.cps.tbdmt.model.Template;
+import org.onap.cps.tbdmt.model.TemplateKey;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExecutionBusinessLogic {
+
+    @Autowired
+    private TemplateRepository templateRepository;
+
+    @Autowired
+    private AppConfiguration appConfiguration;
+
+    @Autowired
+    private CpsRestClient cpsRestClient;
+
+    /**
+     * Execute a template stored in the database.
+     *
+     * @param schemaSet schema set
+     * @param id id
+     * @param executionRequest inputs to be applied to the templates
+     * @return result response from the execution of template
+     */
+    public String executeTemplate(final String schemaSet, final String id, final ExecutionRequest executionRequest) {
+
+        final Optional<Template> templateOptional = templateRepository.findById(new TemplateKey(id, schemaSet));
+        if (templateOptional.isPresent()) {
+            return execute(templateOptional.get(), executionRequest.getInput());
+        }
+        throw new TemplateNotFoundException("Template does not exist");
+    }
+
+    private String execute(final Template template, final Map<String, String> input) {
+        final String anchor = appConfiguration.getSchemaToAnchor().get(template.getModel());
+        if (anchor == null) {
+            throw new ExecuteException("Anchor not found for the schema");
+        }
+        final Jinjava jinja = new Jinjava();
+        final String xpath = jinja.render(template.getXpathTemplate(), input);
+        try {
+            return cpsRestClient.fetchNode(anchor, xpath);
+        } catch (final CpsClientException e) {
+            throw new ExecuteException(e.getLocalizedMessage());
+        }
+    }
+
+}
