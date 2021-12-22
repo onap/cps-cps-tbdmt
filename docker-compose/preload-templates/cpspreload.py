db36560895e39e727ff3825307736f98565ee1a1
Malarvizhi <malarvizhi.44@wipro.com>
2021-12-22 08:06:10 +0000

    Fix bug in preloading templates to CPS-TBDMT container
    Issue-ID: CPS-839
    Signed-off-by: Malarvizhi Paramasivam <malarvizhi.44@wipro.com>
    Change-Id: I3c3cd59d7749fcc823cb34b17366261495b25960

--- docker-compose/preload-templates/cpspreload.py
+++ docker-compose/preload-templates/cpspreload.py
@@ -6,6 +6,7 @@ CPS_HEADERS = {
    "Accept": "application/json",
    "Content-Type": "application/json",
    }
+
 tbdmt_url="http://cps-tbdmt:8080/templates"
  
 
@@ -17,10 +18,9 @@ templates=json.loads(templates_json)
 
 for template in templates:
     print(type(template))
-    print("send rest req")
-    print("waiting before sent req")
-    time.sleep(2)
-    response=requests.post(tbdmt_url, json=template, headers=CPS_HEADERS)
-    print("res",response.reason)
-
+    print("Waiting before sending request..")
+    time.sleep(8)
+    print("Sending rest request..")
+    response=requests.post(tbdmt_url, json=template, headers=CPS_HEADERS, verify=False)
+    print("Response is: ",response.reason)
 
